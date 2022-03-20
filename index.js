9703fa1893286671c7599c561570a206634e64ff
Srinivas <seenuchaparala@gmail.com>
2022-03-21 01:22:45 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let superbowlWin = (record) => {
+    const result = record.find( row => row.result === "W" );
+    return result ? result.year : undefined;
+  }  
+    
+    
+    const record = [
+        {year: "2018", result: "L"},
+        {year: "2017", result: "W"},
+        {year: "2016", result: "N/A"}
+        //...
+      ] 
 
+console.log(superbowlWin(record));
